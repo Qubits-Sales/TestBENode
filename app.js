c113244ccc7583a068cf45af28de515aae61bb84
FabricioZR <jfzr@hotmail.com>
2020-07-25 16:12:51 -0500

    modify starter

--- app.js
+++ app.js
@@ -17,4 +17,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+const http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
