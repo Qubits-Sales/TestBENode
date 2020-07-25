6d167a7a3fd866dd899da5fb23986fa90e3002d4
FabricioZR <jfzr@hotmail.com>
2020-07-25 15:55:36 -0500

    add endpoint test [100%]

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function (req, res, next) {
   res.render('index', {title: 'Express'});
 });
 
+//este endpoint devuelve nombre
+router.get('/test/:name', function (req, res, next) {
+  res.status(200).send('Esto es un test! ' + req.params.name); //TODO add validation on name
+});
+
 module.exports = router;
