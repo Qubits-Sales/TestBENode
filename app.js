4adeb98cdb88d00394ffb510205335588b8e167f
Kechro <kevin.chuquirachi@gmail.com>
2020-07-27 17:25:11 -0500

    project configuration

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,15 +21,17 @@ app.get('/test1', function (req, res, next) {
 	return res.send("exito");
 });
 
-const http = require('http');
+module.exports = app;
 
-/**
- * Get port from environment and store in Express.
- */
-const port = '3001';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
+// const http = require('http');
+//
+// /**
+//  * Get port from environment and store in Express.
+//  */
+// const port = '3001';
+// app.set('port', port);
+//
+// /**
+//  * Create HTTP server.
+//  */
+// const server = http.createServer(app);
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express'});
+	res.render('index', {title: 'Express'});
 });
 
 //este endpoint devuelve nombre
 router.get('/test/:name', function (req, res, next) {
-  res.status(200).send('Esto es un test! ' + req.params.name); //TODO add validation on name
+	res.status(200).send('Esto es un test! ' + req.params.name); //TODO add validation on name
 });
 
 module.exports = router;
